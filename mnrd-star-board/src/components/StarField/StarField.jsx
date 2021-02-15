7ac6a6d052600a540b0c001b65fb7bf46f3580ec
GayofReckoning <afox09@gmail.com>
2021-02-15 09:29:37 -0600

    improved styling

--- mnrd-star-board/src/components/StarField/StarField.jsx
+++ mnrd-star-board/src/components/StarField/StarField.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Stage, Layer, Star, Text, Group} from 'react-konva';
+import { Stage, Layer, Star } from 'react-konva';
 import './StarField.css';
 import {useSelector, useDispatch} from 'react-redux'
 import Popover from '@material-ui/core/Popover';
@@ -9,9 +9,10 @@ import Box from '@material-ui/core/Box';
 export default function StarField (){
   const events = useSelector(state => state.events);
   const dispatch = useDispatch()
-  const [anchorEl, setAnchorEl] = React.useState({attrs:{}});
+  const [anchorEl, setAnchorEl] = React.useState({attrs:{x:0, y:0}});
   const [activity, setActivity] = React.useState({date:''});
 
+  // star info and dragging stored in eventReducer
   const handleDragStart = (e) => {
     const id = e.target.id();
     dispatch({type: 'DRAG_STAR', payload: id})
@@ -24,6 +25,7 @@ export default function StarField (){
   // popover functionality:
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
+    // set the activity related to the star clicked to local state
     for (let thing of events){
       if (thing.id === event.currentTarget.attrs.id){
         setActivity(thing)
@@ -32,15 +34,17 @@ export default function StarField (){
   };
 
   const handleClose = () => {
-    setAnchorEl({attrs:{}});
+    setAnchorEl( { attrs: {x:0, y:0} } );
   };
 
   const open = Boolean(anchorEl.attrs.x);
   const id = open ? 'simple-popover' : undefined;
 
+
   return (
     <div className = "starField">
-    <Stage width={window.innerWidth - 15} height={window.innerHeight-5}>
+    {/* Draggable Stars: */}
+    <Stage width={window.innerWidth} height={window.innerHeight-80}>
       <Layer> 
         {events.map((star) => (
           <Star
@@ -69,6 +73,7 @@ export default function StarField (){
         ))}
       </Layer>
     </Stage>
+    {/* Popover that opens if star is clicked */}
     <Popover
         id={id}
         open={open}
@@ -76,8 +81,8 @@ export default function StarField (){
         anchorPosition={{ top: anchorEl.attrs.y, left: anchorEl.attrs.x }}
         onClose={handleClose}
         anchorOrigin={{
-          vertical: 'center',
-          horizontal: 'right',
+          vertical: 'top',
+          horizontal: 'center',
         }}
         transformOrigin={{
           vertical: 'center',
